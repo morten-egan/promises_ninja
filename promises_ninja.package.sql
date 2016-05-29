444c616081f8b2ccdfc91c577f5cb88bb06371ef
Morten Egan <morten@egan.dk>
2016-05-29 19:57:24 +0800

    prototyping

---
+++ promises_ninja.package.sql
@@ -0,0 +1,13 @@
+create or replace package promises_ninja
+
+as
+
+  /** Package implementing PLSQL equivalent of Javascript promises (https://promisesaplus.com/)
+  * @author Morten Egan
+  * @version 0.0.1
+  * @project promises_ninja
+  */
+  npg_version         varchar2(250) := 0.0.1;
+
+end promises_ninja;
+/
