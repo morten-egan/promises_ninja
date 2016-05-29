99f5a9f2bd80d739ea4675a480d5f4f984f24ca3
Morten Egan <morten@egan.dk>
2016-05-29 20:13:38 +0800

    promise object

--- promises_ninja.package.sql
+++ promises_ninja.package.sql
@@ -9,12 +9,16 @@ as
   */
   npg_version         varchar2(250) := 0.0.1;
 
+  type promise_o is object (
+    aa23        varchar2(20)
+  )
+
   /** The promise function returning an active promise
   * @author Morten Egan
   * @return promise_o The promise object.
   */
   function promise (
-    
+
   )
   return promise_o;
 
